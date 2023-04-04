d388170f4a9ef110598c55f0774f9fe84ae3ead8
Aedan Fish <aedanakfish3@gmail.com>
2023-04-04 20:53:33 +0100

    cleaning up code

--- fruit-search/script.js
+++ fruit-search/script.js
@@ -2,12 +2,10 @@ const input = document.querySelector('#fruit');
 const suggestions = document.querySelector('.suggestions ul');
 const fruitArr = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
 const suggSet = new Set();
-let inputStr = '';
 const fruitSet = new Set(fruitArr);
 
 function search(str) {
 	for (let fruit of fruitSet) {
-		let f = fruit.replaceAll(str, `<b>${str}</b>`);
 		if(fruit.includes(str) || fruit.toLowerCase().includes(str.toLowerCase())) {
 			suggSet.add(fruit);
 		} else if(!fruit.includes(str) && !fruit.toLowerCase().includes(str.toLowerCase())){
@@ -26,17 +24,6 @@ function delList() {
 }
 
 function searchHandler(e) {
-	// if(e.key === 'Backspace') {
-	// 	inputStr = inputStr.substring(0, (inputStr.length-1));
-	// 	if(inputStr === '') {
-	// 		delList();
-	// 		return;
-	// 	}
-	// } else if(!'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM'.includes(e.key)) return;
-	// let letter = e.key
-	// if(e.key !== 'Backspace') {
-	// 	inputStr += letter;
-	// }
 	if(input.value === '') {
 		delList();
 		return;
@@ -63,15 +50,5 @@ function useSuggestion(e) {
 input.addEventListener('keyup', searchHandler);
 suggestions.addEventListener('click', useSuggestion);
 window.addEventListener('keypress', (e) => {
-	if(e.key === "Enter") window.open(`https://www.google.com/search?q=${input.value}+fruit`, target = `_blank`);
+	if(e.key === "Enter" && suggSet.has(input.value)) window.open(`https://www.google.com/search?q=${input.value}+fruit`, target = `_blank`);
 });
-
-function arrowSelect(e) {
-  while (input.value !== "") {
-    if (e.key === "ArrowDown") {
-      const selObj = { ...[...suggSet] };
-      let counter = 0;
-      selObj[counter];
-    }
-  }
-}
